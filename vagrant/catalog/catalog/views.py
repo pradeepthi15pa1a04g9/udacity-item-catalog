4b81dd44b9538a4b5369858878f508718fb049a4
Sam Frances <sam@samfrances.uk>
2016-02-11 23:39:16 +0000

    Remove mock up objects, so all data now from database

--- vagrant/catalog/catalog/views.py
+++ vagrant/catalog/catalog/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, abort, request, session
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 from . import app
 
@@ -6,32 +7,35 @@ from .forms import TagForm, ItemForm
 
 # Imports for dealing with database / models
 from .database import db_session
-from .models import Item, Tag
+from .models import Item as dbItem, Tag as dbTag
 
 # View for basic index page
 
 @app.route('/')
 @app.route('/catalog/')
 def index():
-    return render_template('catalog.html', tags=tags.values(), items=items)
+    tags = db_session.query(dbTag).all()
+    items = db_session.query(dbItem).all()
+    return render_template('catalog.html', tags=tags, items=items)
 
 # Views for viewing data in web page form
 
 @app.route('/catalog/tags/view/<tag_name>/')
 def viewTag(tag_name):
     try:
-        tag = tags[tag_name]
-    except KeyError:
+        tag = db_session.query(dbTag).filter_by(name=tag_name).one()
+    except (MultipleResultsFound, NoResultFound):
         abort(404)
     return render_template('viewtag.html', tag=tag)
 
 @app.route('/catalog/items/view/<item_name>-<int:item_id>/')
 def viewItem(item_name, item_id):
-    if item_name in itemindex and itemindex[item_name].id == item_id:
-        item = itemindex[item_name]
-        return render_template('viewitem.html', item=item, item_id=item_id)
-    else:
+    try:
+        item = db_session.query(dbItem).filter_by(name=item_name, id=item_id).one()
+    except (MultipleResultsFound, NoResultFound):
         abort(404)
+    return render_template('viewitem.html', item=item, item_id=item_id)
+
 
 # Views for creating new data entities
 
@@ -45,7 +49,8 @@ def newTag():
 @app.route('/catalog/items/new/', methods=['GET', 'POST'])
 def newItem():
     form = ItemForm(request.form, meta={'csrf_context': session})
-    form.tags.choices = [(g, g) for g in tags]
+    tags = db_session.query(dbTag).all()
+    form.tags.choices = [(unicode(g.id), g.name) for g in tags]
     if request.method == 'POST' and form.validate():
         return "No code for handling posted forms yet. Here is the request data:<br><br><pre>%s</pre>" % request.values
     return render_template('itemform.html', form=form, action='newItem')
@@ -54,26 +59,36 @@ def newItem():
 
 @app.route('/catalog/tags/edit/<tag_name>/', methods=['GET', 'POST'])
 def editTag(tag_name):
-    form = TagForm(request.form, meta={'csrf_context': session})
+    # Get tag for editing
     try:
-        # Prefill details of edited tag
-        form.tag_name.data = tags[tag_name].name
-    except KeyError:
+        tag = db_session.query(dbTag).filter_by(name=tag_name).one()
+    except (MultipleResultsFound, NoResultFound):
         abort(404)
+
+    form = TagForm(request.form, meta={'csrf_context': session})
+    
     if request.method == 'POST' and form.validate():
         return "No code for handling posted forms yet. Here is the request data:<br><br><pre>%s</pre>" % request.values
-    return render_template('tagform.html', form=form, action="editTag", tag_name=tag_name)    
+    elif request.method == 'GET':
+        # Prefill details of edited tag
+        form.tag_name.data = tag.name
+    return render_template('tagform.html', form=form, action="editTag", tag_name=tag_name)    # TODO: Change to input tag rather than tag name?
 
 @app.route('/catalog/items/edit/<item_name>-<int:item_id>/', methods=['GET', 'POST'])
 def editItem(item_name, item_id):
-    item = itemindex[item_name]
+    try:
+        item = db_session.query(dbItem).filter_by(name=item_name, id=item_id).one()
+    except (MultipleResultsFound, NoResultFound):
+        abort(404)
+
     form = ItemForm(request.form, meta={'csrf_context': session})
-    form.tags.choices = [(g, g) for g in tags]
+    tags = db_session.query(dbTag).all()
+    form.tags.choices = [(unicode(g.id), g.name) for g in tags]
     
     # Prefill details of edited item
     form.name.data = item.name
-    #form.description.data = item.description
-    form.tags.data = [g.name for g in tags.values() if g in item.tags]
+    form.description.data = item.description
+    form.tags.data = [unicode(g.id) for g in tags if g in item.tags]
 
     if request.method == 'POST' and form.validate():
         return "No code for handling posted forms yet. Here is the request data:<br><br><pre>%s</pre>" % request.values
@@ -89,61 +104,3 @@ def deleteTag(tag_name):
 def deleteItem(item_name, item_id):
     return 'This page lets you delete the existing item called "%s" with the id %s.' % (item_name, item_id)
 
-# Mock up objects to make the templates work.
-# REMOVE ONCE PROPER DATABASE IN PLACE:
-
-class Tag(object):
-
-    def __init__(self, name, itemlist):
-        self.name = name
-        self.itemlist = itemlist # The list in which all the items are stored
-
-    @property
-    def items(self):
-        return [item for item in self.itemlist if self in item.tags]
-
-class Item(object):
-
-    def __init__(self, name, id, itemlist, itemindex):
-        self.name = name
-        self.id = id
-        self.tags = []
-        self.itemlist = itemlist
-        self.itemindex = itemindex
-        self.itemlist.append(self)
-        self.itemindex[self.name] = self
-
-    def addTag(self, tag_name):
-        if tag_name in tags.keys():
-            self.tags.append(tags[tag_name])
-
-items = []
-
-itemindex = dict()
-
-tags = {
-    'Cars': Tag('Cars', items),
-    'Bikes': Tag('Bikes', items),
-    'Boats': Tag('Boats', items),
-    'Vehicles': Tag('Vehicles', items),
-}
-
-item1 = Item('Ford Focus', 1, items, itemindex)
-item1.addTag('Cars')
-item1.addTag('Vehicles')
-item1.description = "A really reliable car."
-
-item2 = Item('Rowing boat', 2, items, itemindex)
-item2.addTag('Boats')
-item2.addTag('Vehicles')
-
-item3 = Item('BMX', 3, items, itemindex)
-item3.addTag('Bikes')
-item3.addTag('Vehicles')
-
-item4 = Item('Skoda', 4, items, itemindex)
-item4.addTag('Cars')
-item4.addTag('Vehicles')
-
-item5 = Item('Rover the Dog', 5, items, itemindex)
-item5.description = "A very good dog."
