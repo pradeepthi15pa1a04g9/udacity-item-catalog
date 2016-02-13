a166f10a52f015bb61efa5c2eacc39ace4435e7c
Sam Frances <sam@samfrances.uk>
2016-02-13 14:10:24 +0000

    Minimal login capabilities functioning

--- vagrant/catalog/catalog/views.py
+++ vagrant/catalog/catalog/views.py
@@ -454,7 +454,7 @@ def gconnect():
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % session['username'])
     print "done!"
-    response = make_response(output)
+    response = make_response(json.dumps(output))
     response.headers['Content-Type'] = 'application/json'
     return response
 
