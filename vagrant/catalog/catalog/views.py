46d2a7cffb13b130a46e4e049fdd00feb207a120
Sam Frances <sam@samfrances.uk>
2016-02-15 02:11:18 +0000

    Add nice error pages

--- vagrant/catalog/catalog/views.py
+++ vagrant/catalog/catalog/views.py
@@ -613,3 +613,17 @@ def user_activation(user_id):
                             action=action,
                             user=user,
                             logged_in=True)
+
+# Error pages
+
+@app.errorhandler(404)
+def page_not_found404(e):
+    # Find if logged in, for template info
+    logged_in = 'username' in session
+    return render_template('404.html', logged_in=logged_in), 404
+
+@app.errorhandler(403)
+def page_unauthorised403(e):
+    # Find if logged in, for template info
+    logged_in = 'username' in session
+    return render_template('403.html', logged_in=logged_in), 403
