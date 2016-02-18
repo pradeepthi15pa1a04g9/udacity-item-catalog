fb6bbe1641262b821a8534a94016c3d3bf2851ca
Sam Frances <sam@samfrances.uk>
2016-02-18 08:55:21 +0000

    Calculate absolute path for logging file

--- vagrant/catalog/catalog/config.py
+++ vagrant/catalog/catalog/config.py
@@ -8,7 +8,12 @@ CSRF_SECRET_KEY = b'development csrf key'
 
 # Non-standard, specific to this app
 ENABLE_LOGGING = True
-CATALOG_LOGFILE = 'catalog.log'
+
+CATALOG_LOGFILE = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        'catalog.log'))
 
 # Default sqlite database
 
