5d9b3c5931a48d1d8b198bd7d7dd6a5049c7ea25
Sam Frances <sam@samfrances.uk>
2016-02-18 16:33:09 +0000

    Changed back to old default config that works with built in server. Put wsgi related config in instance config

--- vagrant/catalog/catalog/config.py
+++ vagrant/catalog/catalog/config.py
@@ -9,13 +9,11 @@ CSRF_SECRET_KEY = b'development csrf key'
 # Non-standard, specific to this app
 ENABLE_LOGGING = True
 
-# CATALOG_LOGFILE = os.path.abspath(
-#     os.path.join(
-#         os.path.dirname(__file__),
-#         '..',
-#         'catalog.log'))
-
-CATALOG_LOGFILE = '/tmp/catalog.log'
+CATALOG_LOGFILE = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        'catalog.log'))
 
 # Default sqlite database
 
